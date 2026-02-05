1da8a43479b829bed4d1af0bc5ac624fd19baeac
Yossef-Dawoad <yossefdawoad15@gmail.com>
2026-02-05 05:25:15 -0800

    feat: Implement Stats screen with charts, financial summaries, and core navigation, adhering to MVVM architecture.

--- lib/features/categories/domain/datasources/categories_remote_datasource.dart
+++ lib/features/categories/domain/datasources/categories_remote_datasource.dart
@@ -1,39 +1,37 @@
 import 'package:expancetracker/core/common/domain/intrefaces/remote_base.dart';
+import 'package:pocketbase/pocketbase.dart';
 
 import '../models/transaction_category.dart';
 
 typedef CategoriesRemoteService = RemoteDataBase<TransactionCategory>;
 
 class CategoriesRemoteDataSource implements CategoriesRemoteService {
-  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+  final _store = PocketBase('http://127.0.0.1:8090');
 
   @override
   Future<TransactionCategory> addNewItem(TransactionCategory item) async {
-    await _firestore.collection(collectionName).doc(item.id).set(item.toMap());
+    await _store.collection(collectionName).create(body: item.toMap());
     return item;
   }
 
   @override
   Future<void> deleteItem(TransactionCategory item) async {
-    return await _firestore.collection(collectionName).doc(item.id).delete();
+    return await _store.collection(collectionName).delete(item.id);
   }
 
   @override
   Future<List<TransactionCategory>> getAllItems() async {
-    final dataSnapShot = await _firestore.collection(collectionName).get();
-    final result = dataSnapShot.docs
-        .map((doc) => TransactionCategory.fromMap(doc.data()))
+    final dataSnapShot = await _store.collection(collectionName).getFullList();
+    final result = dataSnapShot
+        .map((doc) => TransactionCategory.fromMap(doc.data))
         .toList();
     return result;
   }
 
   @override
   Future<TransactionCategory> getItemById(String id) async {
-    final dataSnapShot = await _firestore
-        .collection(collectionName)
-        .doc(id)
-        .get();
-    return TransactionCategory.fromMap(dataSnapShot.data()!);
+    final dataSnapShot = await _store.collection(collectionName).getOne(id);
+    return TransactionCategory.fromMap(dataSnapShot.data);
   }
 
   @override
--- lib/features/transactions/domain/datasources/transactions_remote_datasource.dart
+++ lib/features/transactions/domain/datasources/transactions_remote_datasource.dart
@@ -1,39 +1,37 @@
 import 'package:expancetracker/core/common/domain/intrefaces/remote_base.dart';
+import 'package:pocketbase/pocketbase.dart';
 
 import '../models/user_transaction.dart';
 
 typedef UserTransactionServiceType = RemoteDataBase<UserTransaction>;
 
 class TransctionRemoteDataSource implements UserTransactionServiceType {
-  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+  final _store = PocketBase('http://127.0.0.1:8090');
 
   @override
   Future<UserTransaction> addNewItem(UserTransaction item) async {
-    await _firestore.collection(collectionName).doc(item.id).set(item.toMap());
+    await _store.collection(collectionName).create(body: item.toMap());
     return item;
   }
 
   @override
   Future<void> deleteItem(UserTransaction item) {
-    return _firestore.collection(collectionName).doc(item.id).delete();
+    return _store.collection(collectionName).delete(item.id);
   }
 
   @override
   Future<List<UserTransaction>> getAllItems() async {
-    final dataSnapShot = await _firestore.collection(collectionName).get();
-    final result = dataSnapShot.docs
-        .map((doc) => UserTransaction.fromMap(doc.data()))
+    final dataSnapShot = await _store.collection(collectionName).getFullList();
+    final result = dataSnapShot
+        .map((doc) => UserTransaction.fromMap(doc.data))
         .toList();
     return result;
   }
 
   @override
   Future<UserTransaction> getItemById(String id) async {
-    final dataSnapShot = await _firestore
-        .collection(collectionName)
-        .doc(id)
-        .get();
-    return UserTransaction.fromMap(dataSnapShot.data()!);
+    final dataSnapShot = await _store.collection(collectionName).getOne(id);
+    return UserTransaction.fromMap(dataSnapShot.data);
   }
 
   @override
